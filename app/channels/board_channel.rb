a4287fb339f3e182f2bb5eb3f829b7be5ab85e94
Thomas Haddad <thomas.haddad@af83.com>
2017-04-10 21:37:04 +0200

    Add cards updated when action cable gets updated state
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

--- app/channels/board_channel.rb
+++ app/channels/board_channel.rb
@@ -8,7 +8,17 @@ class BoardChannel < ApplicationCable::Channel
   end
 
   def submit(data)
-    ap "submit"
+    cards = []
+    data['state'].each do |col|
+      cards << col['cards']
+    end
+    cards = cards.flatten(1)
+    Card.transaction do
+      cards.each do |card|
+        Card.find(card['id']).update(position: card['position'], column_id: card['column_id'])
+      end
+    end
+
     ActionCable.server.broadcast "board_channel", {state: data['state']}
   end
 end
