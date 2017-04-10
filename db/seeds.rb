be813629c2dee1724a34720207029231a2a9777f
Thomas Haddad <thomas.haddad@af83.com>
2017-04-10 21:58:17 +0200

    Update seed.rb for nicer setup
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Board.delete_all
+
+board = Board.create(title: "K2000 board")
+
+col_1 = Column.create(name: 'A faire', board_id: board.id)
+Column.create(name: 'En cours', board_id: board.id)
+Column.create(name: 'Fait', board_id: board.id)
+
+desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+
+4.times do |n|
+  Card.create(title: "Carte #{n}", description: desc, position: n, column_id:col_1.id)
+end
