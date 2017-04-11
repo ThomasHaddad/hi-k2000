2befe5ecab4955c733ffc0a84887ae1cb42de916
Thomas Haddad <thomas.haddad@af83.com>
2017-04-11 10:23:06 +0200

    Remove card transaction as after commit won't be used
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

--- app/channels/board_channel.rb
+++ app/channels/board_channel.rb
@@ -13,10 +13,8 @@ class BoardChannel < ApplicationCable::Channel
       cards << col['cards']
     end
     cards = cards.flatten(1)
-    Card.transaction do
-      cards.each do |card|
-        Card.find(card['id']).update(position: card['position'], column_id: card['column_id'])
-      end
+    cards.each do |card|
+      Card.find(card['id']).update(position: card['position'], column_id: card['column_id'])
     end
 
     ActionCable.server.broadcast "board_channel", {state: data['state']}
